6cbe0b13e114bec05005132e1d21e48c7a7f9947
thephotogenicbug <navinkumarachari@gmail.com>
2021-06-22 21:08:47 +0530

    Frontend React application: 22-06-2021: initialize get request using localhost and popluate data using map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      citylist:[],
+      booklist:[]
+    }
+  }
+
+getCity = () =>{
+  fetch("http://localhost:8080/")
+  .then(response=>response.json())
+  .then(allcity => this.setState({
+    citylist: allcity
+  }))
+}
+
+getBook = () =>{
+  fetch("http://localhost:8081/")
+  .then(response=>response.json())
+  .then(allbook => this.setState({
+    booklist: allbook
+  }))
+}
+
+componentDidMount(){
+  this.getCity();
+  this.getBook();
+}
+
+ render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>Welcome to React + NodeJS </h1>
+     <h2>Total City : {this.state.citylist.length}</h2>
+     {
+       this.state.citylist.map((cityname, index)=>{
+         return(
+           <p key={index}>{cityname}</p>
+         )
+       })
+     }
+     <h2>Total Books : {this.state.booklist.length}</h2>
+     <table align="center" cellPadding='10'>
+       <thead>
+           <tr>
+             <th>Book Name</th>
+             <th>Book Price</th>
+           </tr>
+       </thead>
+       <tbody>
+       {
+								this.state.booklist.map((book , index)=>{
+								return(
+										<tr key={index}>
+											<td>{book.name}</td>
+											<td>{book.price}</td>
+										</tr>
+									)
+								})
+							}
+
+       </tbody>
+     </table>
+    
+          
     </div>
   );
+ }
 }
 
 export default App;
