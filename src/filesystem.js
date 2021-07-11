6a2aa16821de28ff42155e2c48f14eb9eea93a48
thephotogenicbug <navinkumarachari@gmail.com>
2021-07-11 19:33:36 +0530

    Add clear filesystem and delete filesystem.txt function

--- src/filesystem.js
+++ src/filesystem.js
@@ -30,11 +30,30 @@ const FileSystem = () =>{
    useEffect(()=>{
        getData();
    }, [true])
+
+   const deleteFileData = () =>{
+    var url = "http://localhost:2222/deletefiledata";
+    axios.get(url)
+    .then(response=>{
+        processMessage(response.data);
+        getData();              // to refresh the list after file delete 
+    })
+
+   }
+
+   const deleteFile = () =>{
+    var url = "http://localhost:2222/deletefile";
+    axios.get(url)
+    .then(response=>{
+        processMessage(response.data);
+        getData();              // to refresh the list after file delete 
+    })
+   }
     return(
         <div className="container">
             <div className="row">
                 <div className="col-lg-12  text-center">
-                    <h3 className="mb-4">File Read write using React & NodeJS</h3>
+                    <h3 className="mb-4">File Read write using ReactJS & NodeJS</h3>
                     <p className="text-center text-success"> {message} </p>
                     <textarea className="mt-3"  
                     placeholder="Enter Your Message" 
@@ -42,11 +61,13 @@ const FileSystem = () =>{
                     onChange={obj=> updateData(obj.target.value)}>
                     </textarea>
                     <div >
-                        <button className="btn btn-primary mt-4" onClick={save}>Save Message</button>
+                        <button className="btn btn-primary m-3" onClick={save}>Save Message</button>
                     </div>
                 </div>
-                <div className="col-md-12">
-                    <h2>Data from File System</h2>
+                <div className="col-md-12 text-center">
+                <button className="btn btn-danger m-3" onClick={deleteFileData}>Clear File Data</button>
+                <button className="btn btn-warning m-3" onClick={deleteFile}>Delete File</button>
+                    <h2 className="mt-2">Data from File System</h2>
                     <p>{filedata}</p>
                 </div>
             </div>
